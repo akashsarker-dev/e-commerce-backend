7de91bc89f57b230cd23590ba2edabc28763ba57
akashsarker24 <akashsarkeronline@gmail.com>
2024-01-18 19:00:16 +0600

    email validation

--- controller/registration.js
+++ controller/registration.js
@@ -1,8 +1,9 @@
+const emailValidation = require('../helpers/emailValidation');
 const sendEmail = require('../helpers/sendEmail');
 const UserInfo = require('../models/userSchema')
 const bcrypt = require('bcrypt');
 
-function registration(req,res) {
+async function registration(req,res) {
       const{firstname,lastname,email,telephone,address,city,postcode,division,district, password} = req.body
 
 
@@ -12,12 +13,20 @@ function registration(req,res) {
       if (!email) {
         return res.send({erroe: "email is required"})
       }
+      if (!emailValidation(email)) {
+        return res.json({error : "Email Is not valid"})
+      }
       if (!password) {
         return res.send({erroe: "Please inter you pasword"})
       }
       if (!telephone) {
         return res.send({erroe: "telephone is required"})
       }
+      const existingEmail = await UserInfo.findOne({email})
+        if (existingEmail) {
+          return res.send({error: 'email is already use'})
+        }
+      
 
   console.log(req.body);
   bcrypt.hash(password, 10, function(err, hash) {
