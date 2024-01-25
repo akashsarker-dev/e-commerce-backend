0f51e6761ed90ddf590b0a9f9d20b07f9039aa9f
akashsarker24 <akashsarkeronline@gmail.com>
2024-01-25 20:17:17 +0600

    category list in mongodb

--- controller/createCategoryController.js
+++ controller/createCategoryController.js
@@ -1,6 +1,22 @@
-function createCategoryController(req ,res) {
-    const {name ,description} = req.body;
-    res.send(name)
+const categoryList = require("../models/categorySchema");
+
+async function createCategoryController(req, res) {
+    const { name, description } = req.body;
+
+    const duplicateCategory = await categoryList.find({ name });
+
+    if (duplicateCategory.length > 0) {
+        console.log('Category already exists');
+        res.json({ error: 'This Category Already Exists' })
+    } else {
+        const category = new categoryList({
+            name,
+            description,
+        });
+
+        category.save();
+        res.json({ success: 'Category created successfully' });
+    }
 }
 
-module.exports = createCategoryController
+module.exports = createCategoryController;
